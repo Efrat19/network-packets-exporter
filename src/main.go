a91f44720dc6a0eb46f3b05894881bb114e90fc5
Efrat Levitan <41479945+Efrat19@users.noreply.github.com>
2020-04-29 21:33:39 +0300

    Update main.go

--- src/main.go
+++ src/main.go
@@ -5,6 +5,6 @@ import ("github.com/pkg/profile")
 
 func main() {
 	defer profile.Start().Stop()
-	defer profile.Start(profile.MemProfile).Stop()
+//	defer profile.Start(profile.MemProfile).Stop()
 	capture()
 }
